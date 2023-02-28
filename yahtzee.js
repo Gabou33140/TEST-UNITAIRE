66b8de39f8cfb9c563f6b2c5fa990c1af7ab522b
godillar <gabriel.odillard@outlook.fr>
2023-02-28 22:39:30 +0100

    fin de la refacto, ajout d'un doc html a ameliorer

--- yahtzee.js
+++ yahtzee.js
@@ -13,32 +13,50 @@ bonus => supperieur
 parti mineur > 63 : bonus de 35 pts
 */
 
-// une fonction avec un parametre, il faut une variation ou une
-// exemple : yahtzee.ones du fichier yahtzee.js et l'utiliser dans le test du fichier yahtzee.test.js
+function formatObj(obj,type=""){
+    //formate un objet pour etre proprement afficher en html
+    if (type == "html"){
+        return JSON.stringify(choices).replace(/[\"{}]/g,"").replace(/,/g,"<br/>").replace(/_/g," ");
+    } else {
+        return JSON.stringify(choices).replace(/[\"{}]/g,"").replace(/,/g,"\n").replace(/_/g," ");
+    }
+}
+
+function randomIntFromInterval(min, max) { //
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
 const yahtzee = {
 
-    ones_to_six : function (main,number) {
-        var tab = main.filter(num => num == number) 
+    throw_dace : function() {
+        var list_dace = []
+        for(i = 0; i < 5; i++){
+            list_dace.push(randomIntFromInterval(1,6))
+        }
+        return list_dace;
+    },
+
+    ones_to_six : function (game,number) {
+        var tab = game.filter(num => num == number) 
         return tab.reduce((acc, curr) => acc + curr,0)
     },
     
-    three_four_five : function (main,number) {
+    three_four_five : function (game,number) {
         for (k=1;k<=6;k++){
-            tabk = main.filter(num => num == k)
+            tabk = game.filter(num => num == k)
             if (tabk.length >= number){
                 if (number==5) return 50
-                return main.reduce((acc, curr) => acc + curr,0)
+                return game.reduce((acc, curr) => acc + curr,0)
             }
         }
         return 0
     },
     
 
-    full_house : function (main) {
+    full_house : function (game) {
         var brelan; var pair;
         for (k=1;k<=6;k++){
-            tabk = main.filter(num => num == k)
+            tabk = game.filter(num => num == k)
             if (tabk.length = 2) pair = true
             if (tabk.length = 3) brelan = true
         }
@@ -46,8 +64,8 @@ const yahtzee = {
         return result
     },
 
-    straight : function (main) {
-        var tab_sort = main.sort();
+    straight : function (game) {
+        var tab_sort = game.sort();
         var n = 0;
         for (let k =0; k<tab_sort.length-1;k++){
             if (tab_sort[k+1]==tab_sort[k]+1){
@@ -59,10 +77,50 @@ const yahtzee = {
         return 0
     },
 
-    chance : function (main) {
-        return main.reduce((acc, curr) => acc + curr,0)
+    chance : function (game) {
+        return game.reduce((acc, curr) => acc + curr,0)
     },
 
 }
 
-module.exports = yahtzee;
+function single_play(type_call = ""){
+    game = yahtzee.throw_dace()
+    choices = {  Ones: yahtzee.ones_to_six(game,1),
+                Twos: yahtzee.ones_to_six(game,2),
+                Three: yahtzee.ones_to_six(game,3),
+                Fours: yahtzee.ones_to_six(game,4),
+                Fives: yahtzee.ones_to_six(game,5),
+                Sixes: yahtzee.ones_to_six(game,6),
+                Sum: undefined,
+                Bonus: undefined,
+                Three_of_a_kind : yahtzee.three_four_five(game),
+                Fours_of_a_kind : yahtzee.three_four_five(game),
+                Full_House : yahtzee.full_house(game),
+                Small_Straight : yahtzee.straight(game),
+                Large_Straight : yahtzee.straight(game),
+                Chance : yahtzee.chance(game),
+                YAHTZEE : yahtzee.three_four_five(game),
+                TOTAL_SCORE : undefined
+             }
+    if (type_call == "html"){
+        stringObj = formatObj(choices,"html");
+        return `VOICI VOTRE JEUX : <br/>` + game + `<br/>ET VOTRE GRILLE DE SCORE : <br/>` + stringObj
+    } else {
+        stringObj = formatObj(choices);
+        return `VOICI VOTRE JEUX : \n` + game + `\nET VOTRE GRILLE DE SCORE : \n` + stringObj
+    }
+}
+
+function main_html (){
+    //var life = 2;
+    // while (life != 0) {
+    // }
+    document.write(single_play("html"));  
+}
+
+console.log(single_play());
+
+module.exports = {
+                    yahtzee,
+                 };
+
